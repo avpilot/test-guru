aa632a38696a23de6f7cc5ed5c014ef7e089c541
Andrey Pronin <avpronin86@yandex.ru>
2024-12-19 22:51:43 +0400

    feat: add tests controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :tests do
-    resources :questions
+    resources :questions, shallow: true
   end
 
   get '/tests/:test_id/questions/:id/delete', to: 'questions#delete'
