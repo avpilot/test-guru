25feea976a7a3a566145f7c484069eee314aac3f
Andrey Pronin <avpronin86@yandex.ru>
2025-01-10 00:39:34 +0400

    lesson9-part1

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[new create]
   before_action :find_question, except: %i[new create]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
@@ -6,22 +7,21 @@ class QuestionsController < ApplicationController
   def show; end
 
   def new
-    @question = Question.new
+    @question = @test.questions.new
   end
 
   def edit; end
 
   def destroy
     @question.destroy
-    redirect_to @test
+    redirect_to @question.test
   end
 
   def create
-    test = Test.find(params[:test_id])
-    @question = test.questions.new(question_params)
+    @question = @test.questions.new(question_params)
 
     if @question.save
-      redirect_to test
+      redirect_to @test
     else
       render :new
     end
@@ -29,7 +29,7 @@ class QuestionsController < ApplicationController
 
   def update
     if @question.update(question_params)
-      redirect_to @test
+      redirect_to @question
     else
       render :edit
     end
@@ -41,9 +41,13 @@ class QuestionsController < ApplicationController
     params.require(:question).permit(:body, :test_id)
   end
 
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
   def find_question
     @question = Question.find(params[:id])
-    @test = @question.test
+    # @test = @question.test
   end
 
   def rescue_with_question_not_found
