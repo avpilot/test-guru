d083711df8bd3992024f18078883f2b5c868c797
Andrey Pronin <avpronin86@yandex.ru>
2025-01-12 21:53:48 +0400

    feat: lesson9-task

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,24 +4,38 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_update :before_update_set_next_question
 
   def completed?
     current_question.nil?
   end
 
-  def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
+  def correct_persent
+    (correct_questions.to_f / test.questions.count * 100).round(2)
+  end
 
-    self.current_question = next_question
+  def success?(required_percent = 85)
+    correct_persent >= required_percent
+  end
+
+  def accept!(answer_ids)
+    self.correct_questions += 1 if correct_answer?(answer_ids)
+
     save!
+  end
+
+  def current_question_number
+    test.questions.order(:id).to_a.index(current_question) + 1
   end
 
   private
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
+  end
+
+  def before_update_set_next_question
+    self.current_question = next_question
   end
 
   def correct_answer?(answer_ids)
@@ -38,5 +52,4 @@ class TestPassage < ApplicationRecord
   def next_question
     test.questions.order(:id).where('id > ?', current_question.id).first
   end
-
 end
