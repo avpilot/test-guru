e8af969da5c385f38ae65580804e4228054d848c
Andrey Pronin <avpronin86@yandex.ru>
2025-01-29 20:24:56 +0400

    fix: lesson10_2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,7 @@
 module SessionsHelper
   def show_flash
     flash.map do |type, msg|
-      case type
-      when 'alert' then content_tag :p, msg, class: 'flash alert'
-      else content_tag :p, msg, class: 'flash notice'
-      end
+      content_tag :p, msg, class: "flash #{type}"
     end.join.html_safe
   end
 end
