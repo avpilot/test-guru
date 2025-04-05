3d7136a00326729ea1d5dcd1230ad569d8ab5df2
Andrey Pronin <avpronin86@yandex.ru>
2025-04-05 20:53:37 +0400

    fix: empty password confirmation

--- app/javascript/utilities/checking_password.js
+++ app/javascript/utilities/checking_password.js
@@ -8,7 +8,10 @@ function checkPassword() {
   const password = document.querySelector('input[id="user_password"]').value;
   const password_confirmation = document.querySelector('input[id="user_password_confirmation"]').value;
 
-  if (password_confirmation === password) {
+  if (!password_confirmation) {
+    this.querySelector('.octicon-shield-check').classList.add('hide');
+    this.querySelector('.octicon-shield-x').classList.add('hide');
+  } else if (password_confirmation === password) {
     this.querySelector('.octicon-shield-check').classList.remove('hide');
     this.querySelector('.octicon-shield-x').classList.add('hide');
   } else {
